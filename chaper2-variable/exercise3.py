d4efd45bb2e27edcc9e4912e1cee829171e91fa6
hiscat <1251723871@qq.com>
2021-01-24 14:04:56 +0800

    chapter 2 finish

---
+++ chaper2-variable/exercise3.py
@@ -0,0 +1,6 @@
+# Enter Hours: 35
+# Enter Rate: 2.75
+# Pay: 96.25
+h = input('Enter Hours: ')
+r = input('Enter Rate: ')
+print('Pay', float(h) * float(r))
---
+++ chaper2-variable/exercise5.py
@@ -0,0 +1,4 @@
+# 华氏度 = 32°F+ 摄氏度 × 1.8
+# 摄氏度 = (华氏度 - 32°F) ÷ 1.8
+c = input('Celsius temperature: ')
+print('Fahrenheit temperature: ', 32 + float(c) * 1.8)
